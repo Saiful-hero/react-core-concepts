795442d545bf3d9f0f3edcae12f3f6aa92cba147
Saiful-hero <saif061096@gmail.com>
2023-03-04 19:34:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done cd react-core-concept<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          I'll Learn React Insha-allah
+          </a>
       </header>
     </div>
   );
